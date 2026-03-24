fa301e083f14b63a5db9e204ebcf1cfd769ea8ad
Jorbo1 <johan.bjarbo@gmail.com>
2026-03-24 12:43:02 +0100

    Added CropDetections and TextureCropper

---
+++ XR-Ad_block/Assets/Scripts/CropDetections.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class CropDetections : MonoBehaviour
+{
+    public SentisInferenceManager inferenceManager;
+
+    public event Action<Texture2D> OnCropReady;
+
+    private void OnEnable()
+    {
+        //subscribe to detections from the inference manager
+        inferenceManager.onDetectionsReady += OnDetection;
+    }
+
+    private void OnDisable()
+    {
+        inferenceManager.onDetectionsReady -= OnDetection;
+    }
+
+    private void OnDetection(List<(Rect boundingBox, float confidence, FrameData frame)> detections)
+    {
+        foreach (var detection in detections)
+        {
+            HandleDetection(detection.boundingBox, detection.frame);
+        }
+    }
+
+    private void HandleDetection(Rect boundingBox, FrameData frame)
+    {
+        Texture source = frame.currentTexture;
+        //convert bounding box from normalized to pixel coordinates
+        Rect boundingBoxPixel = new Rect(
+            boundingBox.x * frame.currentResolution.x,
+            boundingBox.y * frame.currentResolution.y,
+            boundingBox.width * frame.currentResolution.x,
+            boundingBox.height * frame.currentResolution.y
+        );
+        Texture2D cropped = TextureCropper.CropBoundingBox(boundingBoxPixel, source);
+
+        OnCropReady?.Invoke(cropped);
+    }
+}
---
+++ XR-Ad_block/Assets/Scripts/TextureCropper.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public static class TextureCropper 
+{
+    //crops the region corresponding to a bounding box using pixel coordinates
+    public static Texture2D CropBoundingBox(Rect boundingBox, Texture source)
+    {
+        int x = (int)boundingBox.x;
+        int y = (int)boundingBox.y;
+        int width = (int)boundingBox.width;
+        int height = (int)boundingBox.height;
+
+        //create destination texture with same size as ROI
+        Texture2D dst = new Texture2D(width, height);
+        Graphics.CopyTexture(
+            source,      
+            0,                  // source mipmap level, not used
+            0,                  // source element, not used
+            x, y, width, height,// source ROI
+            dst, // output texture
+            0,                  // dst mipmap level, not used
+            0,                  // dst element, not used
+            0, 0                // crop offset in dst, 0 because dst and ROI are same size
+        );
+
+        return dst;
+    }
+}
