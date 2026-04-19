1e3fc83643139af2941cb8a074d1ea6e6ee736cb
Pontus Cervin <144095437+MultiMullan@users.noreply.github.com>
2026-04-19 15:24:20 +0200

    Add HUDcontroller and OptionsUI scripts

--- XR-Ad_block/Assets/Scripts/UI/HUDController.cs
+++ XR-Ad_block/Assets/Scripts/UI/HUDController.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using TMPro;
+using Systems.Collections;
 
 public class HUDController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TextMeshProGUI toastText;
+    [SerializeField] private float fadeSpeed = 0.7f;
+
+    private Coroutine _activeToast;
+
+    public IEnumerator ShowToast(string message, float duration)
     {
-        
+        if (_activeToast != null)
+        {
+            StopCoroutine(_activeToast);
+        }
+        _activeToast = StartCoroutine(ToastRoutine(message, duration));
+        yield return _activeToast;
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ToastRoutine(string message, float duration)
     {
-        
+        toastText.text = message;
+
+        //Fade in
+        float alpha = 0f;
+        Color c = toastText.color;
+        while (alpha < 1f)
+        {
+            alpha = Mathf.MoveTowards(alpha, 1f, Time.deltaTime* fadeSpeed);
+            toastText.color = new Color(c.r,c.g,c.b, alpha);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(duration);
+
+        //Fade out
+        while (alpha > 0f)
+        {
+            alpha = Mathf.MoveTowards(alpha, 0f, Time.deltaTime* fadeSpeed);
+            toastText.color = new Color(c.r,c.g,c.b,alpha);
+            yield return null;
+        }
+
+        toastText.text = "";
     }
 }
--- XR-Ad_block/Assets/Scripts/UI/OptionsUI.cs
+++ XR-Ad_block/Assets/Scripts/UI/OptionsUI.cs
@@ -1,16 +1,66 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
 
 public class OptionsUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] private Transform imageButtonContainer;
+    [SerializeField] private GameObject imageButtonPrefab;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private Button resetToDefaultButton;
+
+    private List<Sprite> _loadedSprites = new();
+
+    private void Awake()
     {
-        
+        closeButton.onClick.AddListener(() => gameObject.SetActive(false));
+        resetToDefaultButton.onClick.AddListener(OnResetToDefault);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        LoadImageOptions();
+    }
+
+    private void LoadImageOptions()
+    {
+        // Clear old buttons
+        foreach (Transform child in imageButtonContainer)
+            Destroy(child.gameObject);
+
+        _loadedSprites.Clear();
+
+        // Load all sprites from Resources/BlockerImages
+        Sprite[] sprites = Resources.LoadAll<Sprite>("BlockerImages");
+        _loadedSprites.AddRange(sprites);
+
+        foreach (Sprite sprite in _loadedSprites)
+        {
+            GameObject btn = Instantiate(imageButtonPrefab, imageButtonContainer);
+
+            // Set preview image
+            btn.GetComponentInChildren<Image>().sprite = sprite;
+
+            // Set label to filename
+            btn.GetComponentInChildren<TextMeshProUGUI> ().text = sprite.name;
+
+            // Wire selection
+            Sprite captured = sprite;
+            btn.GetComponent<Button>().onClick.AddListener(() => OnImageSelected(captured));
+        }
+    }
+
+    private void OnImageSelected(Sprite sprite)
+    {
+        BlockerImageSettings.SelectedSprite = sprite;
+        gameObject.SetActive(false);
+    }
+
+    private void OnResetToDefault()
+    {
+        BlockerImageSettings.SelectedSprite = null;
+        gameObject.SetActive(false);
     }
 }
